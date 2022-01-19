c2efcd696d9b3065244b578335bd76b1a40f33a8
Sam Magura <srmagura@gmail.com>
2022-01-19 12:12:59 -0500

    initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace CsvHelperStreamRepro.Controllers;
+
+[Route("api/[controller]/[Action]")]
+public class TestController : ControllerBase
+{
+    // http://localhost:5105/api/test/getPlainText
+    [HttpGet]
+    [Produces("text/plain")]
+    public async Task GetPlainText()
+    {
+        // No `using`: it works
+        // Add `using` to the following line and the response will be empty
+        // Add `await using` to the following line and it will work
+        var sw = new StreamWriter(Response.Body, leaveOpen: true);
+
+        await sw.WriteAsync("Some normal text");
+        await sw.FlushAsync();
+    }
+}
